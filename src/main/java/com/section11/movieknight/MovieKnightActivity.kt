e866d6056f630162068dd17fedaccb9af1c0e6a2
Fede118 <fede.cano118@gmail.com>
2020-09-25 18:11:54 -0300

    created data module

--- src/main/java/com/section11/movieknight/MovieKnightActivity.kt
+++ src/main/java/com/section11/movieknight/MovieKnightActivity.kt
@@ -2,6 +2,9 @@ package com.section11.movieknight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.section11.movieknight.core.MovieKnightModule
+import com.section11.movieknight.core.SchedulerProvider
+import com.section11.movieknight.interactor.MovieKnightInteractor
 import com.section11.movieknight.ui.MovieKnightPresenter
 
 class MovieKnightActivity : AppCompatActivity() {
@@ -11,5 +14,12 @@ class MovieKnightActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val movieKnightModule = MovieKnightModule(applicationContext)
+
+        presenter = MovieKnightPresenter(
+            MovieKnightInteractor(movieKnightModule.getComingSoonMoviesRepository()),
+            SchedulerProvider()
+        )
     }
 }
