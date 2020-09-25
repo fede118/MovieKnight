17efcbdae0ade852562a36bd440b62a29076f411
Fede118 <fede.cano118@gmail.com>
2020-09-25 18:15:05 -0300

    created dtos

---
+++ src/main/java/com/section11/movieknight/dto/Movie.kt
@@ -0,0 +1,75 @@
+package com.section11.movieknight.dto
+
+class Movie constructor(
+    val id: String,
+    val title: String,
+    val fullTitle: String,
+    val year: String,
+    val releaseState: String,
+    val image: String,
+    val runtimeMins: String,
+    val runtimeStr: String,
+    val plot: String,
+    val contentRating: String,
+    val imDbRating: String,
+    val metacriticRating: String,
+    val genres: String,
+    val directors: String,
+    val stars: String
+)
+
+//"id": "tt5918982",
+//"title": "Possessor Uncut",
+//"fullTitle": "Possessor Uncut (2020)",
+//"year": "2020",
+//"releaseState": "October 2",
+//"image": "https://imdb-api.com/images/original/MV5BMjBlYTlhMDctNTQ3ZC00MjM4LWIxNjctNDI4NjA3NDc3Yzc5XkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_Ratio0.6890_AL_.jpg",
+//"runtimeMins": "103",
+//"runtimeStr": "1h 43mins",
+//"plot": "Possessor follows an agent who works for a secretive organization that uses brain-implant technology to inhabit other people's bodies - ultimately driving them to commit assassinations for high-paying clients.",
+//"contentRating": "",
+//"imDbRating": "",
+//"imDbRatingCount": "",
+//"metacriticRating": "69",
+//"genres": "Horror, Sci-Fi, Thriller",
+//"genreList": [
+//{
+//    "key": "Horror",
+//    "value": "Horror"
+//},
+//{
+//    "key": "Sci-Fi",
+//    "value": "Sci-Fi"
+//},
+//{
+//    "key": "Thriller",
+//    "value": "Thriller"
+//}
+//],
+//"directors": "Brandon Cronenberg",
+//"directorList": [
+//{
+//    "id": "nm0188722",
+//    "name": "Brandon Cronenberg"
+//}
+//],
+//"stars": "Andrea Riseborough, Christopher Abbott, Jennifer Jason Leigh, Sean Bean",
+//"starList": [
+//{
+//    "id": "nm2057859",
+//    "name": "Andrea Riseborough"
+//},
+//{
+//    "id": "nm3571592",
+//    "name": "Christopher Abbott"
+//},
+//{
+//    "id": "nm0000492",
+//    "name": "Jennifer Jason Leigh"
+//},
+//{
+//    "id": "nm0000293",
+//    "name": "Sean Bean"
+//}
+//]
+//}
---
+++ src/main/java/com/section11/movieknight/dto/MovieKnightResponse.kt
@@ -0,0 +1,3 @@
+package com.section11.movieknight.dto
+
+class ComingSoonResponse constructor(val items : List<Movie>)
