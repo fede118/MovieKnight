6f495e5b455a2200ab79bd124919cccf2d8cd7e9
Fede118 <fede.cano118@gmail.com>
2020-09-25 18:15:20 -0300

    created scheduler provider wrapper

---
+++ src/main/java/com/section11/movieknight/core/SchedulerProvider.kt
@@ -0,0 +1,16 @@
+package com.section11.movieknight.core
+
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+class SchedulerProvider {
+
+    fun io() : Scheduler {
+        return Schedulers.io()
+    }
+
+    fun ui() : Scheduler {
+        return AndroidSchedulers.mainThread()
+    }
+}
