eb1fd4191d83ca0139657fcfeedcb1d1765195c7
Fede118 <fede.cano118@gmail.com>
2021-06-24 13:20:39 -0300

    refactored MovieKnightResponse.kt for coroutines

--- src/main/java/com/section11/movieknight/dto/MovieKnightResponse.kt
+++ src/main/java/com/section11/movieknight/dto/MovieKnightResponse.kt
@@ -4,10 +4,18 @@ import com.google.gson.annotations.SerializedName
 
 class ComingSoonResponse constructor(
     @SerializedName("items")
-    val movies : List<Movie>
-)
+    val movies : List<Movie>?
+) {
+    companion object CREATOR {
+        @JvmStatic val EMPTY = ComingSoonResponse(null)
+    }
+}
 
 class InTheatersResponse constructor(
     @SerializedName("items")
-    val movies : List<Movie>
-)
+    val movies : List<Movie>?
+) {
+    companion object CREATOR {
+        @JvmStatic val EMPTY = InTheatersResponse(null)
+    }
+}
